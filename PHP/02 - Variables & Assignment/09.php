d669e82da640aa1a6889ecab8829e01b0fd0b5a6
Adam Garali <adam.garali@gmail.com>
2022-09-15 14:06:49 +0200

    addded .

--- PHP/02 - Variables & Assignment/09.php
+++ PHP/02 - Variables & Assignment/09.php
@@ -3,4 +3,4 @@ $name = 'Alicia Vikander';
 $yearOfBirth = 1988;
 $currentYear = date('Y');
 
-echo $name . ' was born in ' . $yearOfBirth . ' and is ' . ($currentYear - $yearOfBirth) . ' years old';
+echo $name . ' was born in ' . $yearOfBirth . ' and is ' . ($currentYear - $yearOfBirth) . ' years old.';
