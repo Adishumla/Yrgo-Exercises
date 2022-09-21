0cd303916f68771efbfd07f0bdfa469c81e3fa21
Adam Garali <adam.garali@gmail.com>
2022-09-21 13:05:34 +0200

    added better version of greeter

--- PHP/04 - Logic & Presentation/04.php
+++ PHP/04 - Logic & Presentation/04.php
@@ -1,6 +1,12 @@
 <?php
-include_once 'actors.php';
+include_once "actors.php";
 
 foreach ($actors as $actor) {
-    echo '<a href="' . $actor['tmdb_url'] . '">' . $actor['name'] . '</a> - ' . $actor['character'] . '<br>';
+    echo '<a href="' .
+        $actor["tmdb_url"] .
+        '">' .
+        $actor["name"] .
+        "</a> - " .
+        $actor["character"] .
+        "<br>";
 }
---
+++ PHP/Extra-Exercises/PHP-Greeter/Greeter.php
@@ -0,0 +1,28 @@
+<?php 
+function greet() {
+    $name = $_REQUEST['name']; // $_REQUEST is a superglobal variable that contains all the data from the form
+    $time = (int) $_REQUEST['time']; // (int) casts the value to an integer
+    switch ($time) {
+        case $time >= 0 && $time <= 11:
+            echo "Good morning, $name!";
+            break;
+        case $time >= 12 && $time <= 17:
+            echo "Good afternoon, $name!";
+            break;
+        case $time >= 18 && $time <= 24:
+            echo "Good evening, $name!";
+            break;
+        default:
+            echo "Hello, $name!";
+            break;
+    }
+}
+?>
+<form action="Greeter.php" method="post">
+    <label for="name">What is your name?</label>
+    <input type="text" name="name" id="name">
+    <label for="time">What time of day is it?</label>
+    <input type="text" name="time" id="time">
+    <input type="submit" value="Submit">
+</form>
+<?php greet() ?>
