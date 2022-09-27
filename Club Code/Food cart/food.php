76e038aaac0301d5e19645e2946eed6581c51c08
Adam Garali <adam.garali@gmail.com>
2022-09-27 09:52:55 +0200

    Commented food

--- Club Code/Food cart/food.php
+++ Club Code/Food cart/food.php
@@ -4,7 +4,7 @@
 $cart = '';
 $money = 100;
 
-$food = [
+$food = [ // array of food
     [
         'name' => 'lettuce',
         'price' => 13,
@@ -51,16 +51,16 @@ $food = [
     ],
 
 ];
-while ($money > 0) {
-    $randomFood = $food[array_rand($food)];
-    if ($money >= $randomFood['price']) {
-        $money -= $randomFood['price'];
-        $cart .= $randomFood['name'] . ', ';
+while ($money > 0) { // while loop to keep the game going until the money is gone
+    $randomFood = $food[array_rand($food)]; // random food from the array
+    if ($money >= $randomFood['price']) { // if the money is greater than the price of the food
+        $money -= $randomFood['price']; // subtract the price of the food from the money
+        $cart .= $randomFood['name'] . ', '; // add the food to the cart
     }
-    if ($money < $randomFood['price']) {
-        unset($food[array_search($randomFood, $food)]);
+    if ($money < $randomFood['price']) { // if money is less than the price of the random food, stop buying
+        unset($food[array_search($randomFood, $food)]); // remove the food from the array
     }
-    if (count($food) == 0) {
+    if (count($food) == 0) { // if there is no more food in the array, stop buying
         break;
     }
 }
