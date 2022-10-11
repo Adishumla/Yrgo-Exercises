c248afda4b1a46f84297c613eb682e87ea826e15
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-10-11 08:30:42 +0200

    Update all-solutions.sql

--- SQLite/03 - Multi-table queries with JOINs/all-solutions.sql
+++ SQLite/03 - Multi-table queries with JOINs/all-solutions.sql
@@ -1,6 +1,10 @@
 1. SELECT id AS Trams FROM trams WHERE color LIKE 'blue'
+
 2. SELECT trams.id, stations."name" FROM trams INNER JOIN stations on stations.id = trams.station_id
-3. SELECT stations."name", count(trams.station_id) FROM trams INNER JOIN stations ON stations.id = trams.station_id GROUP BY "name"
+
+3. SELECT stations."name", count(trams.station_id) FROM trams 
+INNER JOIN stations ON stations.id = trams.station_id GROUP BY "name"
+
 4. SELECT stop_tram.tram_id as id, trams.color, stops."name" from stop_tram 
 INNER JOIN trams on stop_tram.tram_id = trams.id
 INNER JOIN stops on stops.id = stop_tram.stop_id WHERE stop_tram.tram_id LIKE 11
