f2f22828769f8da248517812be34faa738dcc243
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-11-09 08:22:29 +0100

    added

--- JavaScript/05 - Freeze 101/03/script.js
+++ JavaScript/05 - Freeze 101/03/script.js
@@ -1,69 +1,18 @@
-const list = document.querySelector("ul");
-const input = document.querySelector(".l1");
-const submit = document.querySelector(".submit");
-const name = document.querySelector(".name");
-const duration = document.querySelector(".duration");
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const ul = document.querySelector(".tasks");
+const addTask = document.querySelector(".btn");
+const name = document.querySelector(".name").value;
+const duration = document.querySelector(".duration").value;
 
+//task object with name, duration and isDone properties
 const task = {
-  name: "do laundry",
-  duration: 30,
-  isDone() {
-    return "done";
-  },
+  name: "",
+  duration: 0,
+  isDone: false,
 };
 
-const tasks = [
-  {
-    name: "do laundry",
-    duration: 30,
-    isDone() {
-      return "done";
-    },
-  },
-  {
-    name: "do dishes",
-    duration: 15,
-    isDone() {
-      return "done";
-    },
-  },
-  {
-    name: "do homework",
-    duration: 120,
-    isDone() {
-      return "done";
-    },
-  },
-];
+//array to store tasks
+const tasks = [];
 
-//add all tasks DOM
-tasks.forEach((task) => {
-  const li = document.createElement("li");
-  li.innerHTML = `${task.name} - ${task.duration} minutes`;
-  list.appendChild(li);
-});
-
-/* for (let i = 0; i < tasks.length; i++) {
-  const task = tasks[i];
-  const li = document.createElement("li");
-  li.classList.add("task");
-  li.innerHTML = `<span class="name">${task.name}</span> <span class="duration">${task.duration}</span>`;
-  list.appendChild(li);
-} */
-
-/* input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    const item = document.createElement("li");
-    item.textContent = input.value;
-    list.appendChild(item);
-    input.value = "";
-  }
-}); */
-
-/* submit.addEventListener("click", () => {
-  const item = document.createElement("li");
-  item.textContent = `${name.value} - ${duration.value} minutes`;
-  list.appendChild(item);
-  name.value = "";
-  duration.value = "";
-}); */
+//event listener for add task button
