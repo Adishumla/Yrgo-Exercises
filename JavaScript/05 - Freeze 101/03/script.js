7701fd8dc53d989ae742989dd8b8087783be0838
Adishumla <45212964+Adishumla@users.noreply.github.com>
2022-11-10 08:43:06 +0100

    all but extra done

--- JavaScript/05 - Freeze 101/03/script.js
+++ JavaScript/05 - Freeze 101/03/script.js
@@ -1,18 +1,49 @@
-const form = document.querySelector(".form");
-const input = document.querySelector(".input");
-const ul = document.querySelector(".tasks");
-const addTask = document.querySelector(".btn");
-const name = document.querySelector(".name").value;
-const duration = document.querySelector(".duration").value;
+const tasks = [
+  { name: "Do the dishes", duration: 50, isDone: true },
+  { name: "Walk the dog", duration: 20, isDone: false },
+  { name: "Take a nap", duration: 60, isDone: false },
+];
 
-//task object with name, duration and isDone properties
-const task = {
-  name: "",
-  duration: 0,
-  isDone: false,
+const form = document.querySelector("form");
+const tasksElement = document.querySelector(".tasks");
+
+const initialize = () => {
+  tasks.forEach(createTask);
+
+  form.addEventListener("submit", onFormSubmit);
 };
 
-//array to store tasks
-const tasks = [];
+const createTask = (task) => {
+  const div = document.createElement("div");
 
-//event listener for add task button
+  div.innerHTML = `<div class="task">
+      <p>Name: ${task.name} </p>
+      <p>Duration: ${task.duration} minutes </p>
+      <label for="isDone"> Is done </label>
+      <input type="checkbox" id="isDone" name="isDone" ${
+        task.isDone ? "checked" : ""
+      }/>  
+  </div>`;
+
+  // Only prepend the first child. It will then skip the first div
+  tasksElement.prepend(div.firstChild);
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  createTask(data);
+
+  form.reset(); // Built in function on a form element
+};
+
+//when isDone is checked, the task is done  and the task is removed from the list
+const onIsDoneChange = (e) => {
+  const task = e.target.closest(".task");
+  task.remove();
+};
+
+initialize(); // Run code
