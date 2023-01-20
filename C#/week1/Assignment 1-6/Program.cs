cc1bd02c667710e7158374dd4a0d67c71c12f84f
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-01-20 12:58:35 +0100

    added Assignment 1-6

---
+++ C#/week1/Assignment 1-6/Program.cs
@@ -0,0 +1,18 @@
+﻿var start = 3;
+var stop = 1278;
+
+// TODO: What is this I don't even
+/*do
+{
+    Console.WriteLine(start++);
+
+    if ((start = start + start) > stop)
+        break;
+}
+while(true);*/
+
+for (int i = start; i <= stop; i++)
+{
+    i = (i + i);
+    Console.WriteLine(i);
+}
