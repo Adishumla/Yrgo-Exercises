84fc6718e754956e3be4c47b992d7642f32cdc57
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-01 12:01:30 +0100

    lesson 1 complete

---
+++ Wordpress/sourscoops/app/public/wp-content/themes/sourscoops/index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+
+<?php if (have_posts()) : ?>
+
+    <?php while (have_posts()) : the_post();
+        print_a($post);
+    endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
+
