eeff66587edf5b39a48bc7ce999ae19223cbd413
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-02-23 19:46:34 +0100

    .

--- Wordpress/sourscoops/app/public/wp-content/themes/sourscoops/functions.php
+++ Wordpress/sourscoops/app/public/wp-content/themes/sourscoops/functions.php
@@ -9,11 +9,18 @@ function load_tailwind()
 }
 add_action('wp_enqueue_scripts', 'load_tailwind');
 
+function editor_enqueue()
+{
+    wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
+    // Load tailwind.config.js after tailwind has been loaded
+    wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
+}
+add_action('enqueue_block_editor_assets', 'editor_enqueue');
+
 // Register theme support
 add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     add_theme_support('menus');
-    add_theme_support('post-thumbnails');
 });
 
 // Register theme menu(s)
@@ -22,12 +29,29 @@ function register_theme_menus()
 {
     register_nav_menus(array(
         'primary-menu' => __('Primary Menu'),
+        'secondary-menu' => __('Secondary Menu'),
         'footer-menu' => __('Footer Menu'),
     ));
 }
 add_action('init', 'register_theme_menus');
 
-// Print array or object in a pre tag
+// Register Custom Post Type "Ice Cream"
+// https://developer.wordpress.org/themes/functionality/custom-post-types/
+function create_post_type()
+{
+    register_post_type('ice-cream', array(
+        'labels' => array(
+            'name' => __('Ice Cream'),
+            'singular_name' => __('Ice Cream'),
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'ice-cream'),
+        'menu_icon' => 'dashicons-smiley'
+    ));
+}
+add_action('init', 'create_post_type');
+
 function print_a($data)
 {
 ?>
@@ -36,15 +60,6 @@ function print_a($data)
 }
 
 add_action('init', function () {
-    // create a new custom post called ice cream and add it to the admin menu with a custom icon
-    register_post_type('ice-cream', array(
-        'labels' => array(
-            'name' => __('Ice Cream'),
-            'singular_name' => __('Ice Cream'),
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'menu_icon' => 'dashicons-thumbs-down',
-        'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'custom-fields'),
-    ));
+    register_block_type(__DIR__ . '/blocks/hero');
+    register_block_type(__DIR__ . '/blocks/vendors');
 });
--- Wordpress/sourscoops/app/public/wp-content/themes/sourscoops/header.php
+++ Wordpress/sourscoops/app/public/wp-content/themes/sourscoops/header.php
@@ -1,3 +1,8 @@
+<?php
+$primary_menu = wp_get_nav_menu_items('primary-menu');
+$secondary_menu = wp_get_nav_menu_items('secondary-menu');
+$current_page_id = get_queried_object_id();
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
@@ -13,25 +18,20 @@
 
 <body <?php body_class("bg-pink-light text-brown"); ?>>
     <?php wp_body_open(); ?>
-    <?php $menuItems = wp_get_nav_menu_items('primary-menu'); ?>
-
     <nav class="font-mono border-b-2 border-brown text-lg px-8">
         <div class="max-w-7xl mx-auto grid grid-cols-12">
             <div class="col-span-5 py-5 flex gap-10 items-center">
-                <?php $currentPageId = get_queried_object_id();
-                if ($menuItems) foreach ($menuItems as $item) : ?>
-                    <a title="<?= $item->title; ?>" class="<?= $currentPageId == $item->object_id ? 'underline' : '' ?>" href="<?= $item->url; ?>">
-                        <?= $item->title; ?>
-                    </a>
+                <?php if ($primary_menu) foreach ($primary_menu as $link) : ?>
+                    <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>" href="<?= $link->url; ?>"><?= $link->title; ?></a>
                 <?php endforeach; ?>
             </div>
             <div class="col-span-2 flex justify-center items-center py-3">
-                <a href="<?= site_url(); ?>"><img class="h-16 w-auto" src="<?= get_template_directory_uri(); ?>/src/images/sour-scoops-logo.svg" />
-                </a>
+                <a href="/"><img class="h-16 w-auto" src="<?= get_template_directory_uri(); ?>/src/images/sour-scoops-logo.svg" /></a>
             </div>
             <div class="col-span-5 flex justify-end items-center py-5">
-                <a href="<?= site_url(); ?>/shop" class="bg-brown text-pink-light px-4 py-2 rounded-xl">Get one now 👉</a>
-
+                <?php if ($secondary_menu) foreach ($secondary_menu as $link) : ?>
+                    <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>" href="<?= $link->url; ?>"><?= $link->title; ?></a>
+                <?php endforeach; ?>
             </div>
         </div>
     </nav>
