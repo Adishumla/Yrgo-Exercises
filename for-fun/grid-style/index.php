42ae6aad6215284a4a13cebc9a378b467adcb1a4
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-03-20 18:02:08 +0100

    grid-style

---
+++ for-fun/grid-style/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <section class="grid grid-cols-2 gap-4 w-3/4 mx-auto my-10">
+        <div class="flex flex-col gap-4">
+            <img src="/img/alvan-nee-lvFlpqEvuRM-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/cat-han-aPD1yNUYP0w-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/vishwajeet-nishad-TZXusEq5AiU-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/amirali-mirhashemian-XtLPfib7OuM-unsplash.jpg " alt="" class="w-full h-auto">
+        </div>
+        <div class="flex flex-col gap-4">
+            <img src="/img/amirali-mirhashemian-XtLPfib7OuM-unsplash.jpg " alt="" class="w-full h-auto">
+            <img src="/img/vishwajeet-nishad-TZXusEq5AiU-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/alvan-nee-lvFlpqEvuRM-unsplash.jpg" alt="" class="w-full h-auto">
+            <img src="/img/cat-han-aPD1yNUYP0w-unsplash.jpg" alt="" class="w-full h-auto">
+        </div>
+    </section>
+</body>
+
+</html>
